217bd1a5cb6bf12c722538851899c9be26d9ddbd
支 <39434973+iamzhiyudong@users.noreply.github.com>
2019-05-02 12:02:52 +0800

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,25 @@
+var jiexi1 = function() {
+    var url = document.getElementById("text").value;
+    var iframe = document.getElementById("iframe_box");
+    iframe.src = "http://74t.top/?url=" + url;
+}
+var jiexi2 = function() {
+    var url = document.getElementById("text").value;
+    var iframe = document.getElementById("iframe_box");
+    iframe.src = "http://ckplay.online/?url=" + url;
+}
+var jiexi3 = function() {
+    var url = document.getElementById("text").value;
+    var iframe = document.getElementById("iframe_box");
+    iframe.src = "http://mimijiexi.top/?url=" + url;
+}
+var jiexi4 = function() {
+    var url = document.getElementById("text").value;
+    var iframe = document.getElementById("iframe_box");
+    iframe.src = "http://api.baiyug.vip/index.php?url=" + url;
+}
+var jiexi5 = function() {
+    var url = document.getElementById("text").value;
+    var iframe = document.getElementById("iframe_box");
+    iframe.src = "https://api.52xmw.com/?url=" + url;
+}
